e2accede3dbb1ad58a044f830f3e0e677d46c4b6
saepp <dsrisk911@gmail.com>
2021-07-25 01:13:00 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ const response = require("./res");
 const connection = require("./koneksi");
 
 exports.index = function (req, res) {
-  response.ok("Aplikasi REST API berjalan!", res);
+  response.ok("Aplikasi REST API ku berjalan!", res);
+};
+
+// menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
 };
--- routes.js
+++ routes.js
@@ -4,4 +4,5 @@ module.exports = function (app) {
   const jsonku = require("./controller");
 
   app.route("/").get(jsonku.index);
+  app.route("/tampil").get(jsonku.tampilsemuamahasiswa);
 };
