610904e07f3e8b43ccba8eebe3a899e5930cbd0d
saepp <dsrisk911@gmail.com>
2021-07-25 01:28:45 +0700

    berhasil tambah data

--- routes.js
+++ routes.js
@@ -6,4 +6,5 @@ module.exports = function (app) {
   app.route("/").get(jsonku.index);
   app.route("/tampil").get(jsonku.tampilsemuamahasiswa);
   app.route("/tampil/:id").get(jsonku.tampilberdasarkanid);
+  app.route("/tambah").post(jsonku.tambahMahasiswa);
 };
