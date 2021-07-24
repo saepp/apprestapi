329869f6a1e6d35b1c57f85df993c7dba2f49eaa
saepp <dsrisk911@gmail.com>
2021-07-25 01:47:46 +0700

    berhasil hapus data

--- routes.js
+++ routes.js
@@ -8,4 +8,5 @@ module.exports = function (app) {
   app.route("/tampil/:id").get(jsonku.tampilberdasarkanid);
   app.route("/tambah").post(jsonku.tambahMahasiswa);
   app.route("/ubah").put(jsonku.ubahMahasiswa);
+  app.route("/hapus").delete(jsonku.hapusMahasiswa);
 };
