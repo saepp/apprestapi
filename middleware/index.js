b161deed135db3eeecdf13e858841d7eb32e88e5
saepp <dsrisk911@gmail.com>
2021-07-25 15:58:35 +0700

    oke role beres

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const auth = require("./auth");
 const router = express.Router();
+const verifikasi = require("./verifikasi");
 
 // daftarkan menu registrasi
 router.post("/api/v1/register", auth.registrasi);
 router.post("/api/v1/login", auth.login);
+
+// alamat yang perlu otorisasi
+router.get("/api/v1/halamanrahasia", verifikasi(2), auth.halamanrahasia);
 
 module.exports = router;
