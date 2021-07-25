44cbfce861b44fa149c3e95e63edf6a42390296a
saepp <dsrisk911@gmail.com>
2021-07-25 15:21:08 +0700

    login user

--- middleware/auth.js
+++ middleware/auth.js
@@ -42,3 +42,56 @@ exports.registrasi = function (req, res) {
     }
   });
 };
+
+// controller untuk login
+exports.login = function (req, res) {
+  const post = {
+    password: req.body.password,
+    email: req.body.email,
+  };
+
+  let query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+  const table = ["user", "password", md5(post.password), "email", post.email];
+
+  query = mysql.format(query, table);
+  connection.query(query, function (error, rows) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows.length == 1) {
+        let token = jwt.sign({ rows }, config.secret, {
+          expiresIn: 1440,
+        });
+        id_user = rows[0].id;
+
+        const data = {
+          id_user,
+          access_token: token,
+          ip_address: ip.address(),
+        };
+
+        let query = "INSERT INTO ?? SET ?";
+        const table = ["akses_token"];
+
+        query = mysql.format(query, table);
+        connection.query(query, data, function (error, rows) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.json({
+              success: true,
+              message: "Token JWT ter generate",
+              token,
+              currUser: data.id_user,
+            });
+          }
+        });
+      } else {
+        res.json({
+          error: true,
+          message: "Email / Password salah!",
+        });
+      }
+    }
+  });
+};
